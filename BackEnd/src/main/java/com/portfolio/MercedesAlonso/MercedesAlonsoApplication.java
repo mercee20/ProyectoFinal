b2884b25716ea74c86435d23add4819ece3be2fb
mercee20 <99300945+mercee20@users.noreply.github.com>
2022-08-03 17:27:38 -0300

    BackEnd

---
+++ BackEnd/src/main/java/com/portfolio/MercedesAlonso/MercedesAlonsoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.MercedesAlonso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercedesAlonsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercedesAlonsoApplication.class, args);
+	}
+
+}
